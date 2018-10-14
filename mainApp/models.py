574c31f0113558f711625bbf9938d75c91f6e54e
Amos Lucian Isaila <amosisa700@gmail.com>
2018-10-14 20:31:03 +0200

    Update models.py

--- mainApp/models.py
+++ mainApp/models.py
@@ -3,9 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class TimeStampedModel(models.Model):
+    """
+    An abstract base class model that provides selfupdating
+    ``created`` and ``modified`` fields.
+    """
 
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
-class Video(models.Model):
+    class Meta:
+        abstract = True
+
+class Video(TimeStampedModel):
     VideoID = models.IntegerField
     Path = models.CharField(max_length=100, null = False, blank = False)
     Title = models.CharField(max_length=50, null = False, blank = False)
@@ -15,7 +25,7 @@ class Video(models.Model):
 
 
 
-class Tag(models.Model):
+class Tag(TimeStampedModel):
     TagID = models.IntegerField
     Name = models.CharField(max_length=50, null=False, blank=False)
     Video = models.ForeignKey(Video, on_delete=models.CASCADE)
